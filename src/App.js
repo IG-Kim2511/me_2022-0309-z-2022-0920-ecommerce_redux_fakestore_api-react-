6d6941f1109b9147b2c183c1e1e621747a163ad7
IG <iikim2511@gmail.com>
2022-09-20 22:08:29 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+// import './App.css';
+
+/* 
+  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +16,11 @@ function App() {
           Learn React
         </a>
       </header>
+  */
+function App() {
+  return (
+    <div className="App">
+      hi
     </div>
   );
 }
